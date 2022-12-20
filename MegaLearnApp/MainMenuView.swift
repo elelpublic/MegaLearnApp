3a2fa7f44b4e46630e87306c76ff268b05d837b3
Lars Löffler <elelpublic@gmail.com>
2022-12-20 12:56:05 +0100

    Macos specific file.

---
+++ MegaLearnApp/MainMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MainMenuView.swift
+//  MegaLearnApp
+//
+//  Created by Lars Löffler on 20.12.22.
+//
+
+import SwiftUI
+
+struct MainMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainMenuView()
+    }
+}
